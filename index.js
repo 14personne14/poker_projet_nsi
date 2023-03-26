70010c864d8be79d619d892e3764033ebf70bf03
eloilag <eloi.lag5@gmail.com>
2023-03-26 16:48:12 +0200

    connexion - inscription

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 // Require
 const express = require('express');
 const http = require('http');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const sqlite3 = require('sqlite3').verbose();
 const colors = require('colors');
 
 // Vaiables constantes
 const app = express();
 const server = http.createServer(app);
 const port = 8101;
+const regex_username = /^[a-zA-Z0-9]+_?[a-zA-Z0-9]*$/;
+const regex_password = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
 
-app.use(express.static(__dirname)); // Gère les fichiers du dossier "/public"
+// Connexion à la base de données
+const database = new sqlite3.Database('./database/database.db');
 
+// Gère les fichiers du dossier "/public"
+app.use(express.static(__dirname));
+
+// Pour les données qui passent en POST
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
+
+// Gère le language EJS
 app.set('view engine', 'ejs');
+
+// Fonctions :
 
 /*
  *
@@ -23,18 +42,70 @@ app.set('view engine', 'ejs');
  *
  */
 
-// Pour la connexion au serveur
-app.post('/', (req, res) => {
-	// Récupérez les données
+// Pour la connexion
+app.post('/connexion', (req, res) => {
+	// Récupérer les données
 	const data = req.body;
-	
-    // Verification information 
-    
+
+	// Verification information
+	var password_md5 = crypto.createHash('md5').update(data.password).digest('hex');
+
+	// Requete
+	database.all(`SELECT * FROM utilisateur WHERE username = '${data.username}' AND password = '${password_md5}'; `, (err, rows) => {
+		if (err) {
+			console.log(err);
+			res.send('Erreur lors de la connexion.');
+		} else if (rows.length > 0) {
+			res.send(`Connexion Réussie : ${rows[0].username}`);
+		} else {
+			res.send("Nom d'utilisateur ou mot de passe incorrect !");
+		}
+	});
+});
+
+// Pour l'inscription
+app.post('/inscription', (req, res) => {
+	// Récupérer les données
+	const data = req.body;
+
+	// Verif des regex
+	var correct = Boolean(regex_username.exec(data.username)) && Boolean(regex_password.exec(data.password));
+
+	// Hash du mot de passe
+	var password_md5 = crypto.createHash('md5').update(data.password).digest('hex');
+
+	if (correct) {
+		// Requete de vérification
+		database.all(`SELECT * FROM utilisateur WHERE username = '${data.username}'; `, (err, rows) => {
+			if (err) {
+				console.log(err);
+				res.send('Erreur lors de la connexion.');
+			} else if (rows.length > 0) {
+				res.send(`Nom d'utilisateur deja prit !`);
+			} else {
+				// Insertion dans la base de donnée
+				database.all(`INSERT INTO utilisateur (username, password) VALUES ('${data.username}', '${password_md5}'); `, (err, rows) => {
+					if (err) {
+						console.log(err);
+						res.send("Erreur lors de l'insertion.");
+					} else {
+						res.send(`Inscription Réussie !`);
+					}
+				});
+			}
+		});
+	} else {
+		res.send("Mauvais mot de passe ou nom d'utilisateur ");
+	}
 });
 
 // Quand le client demande '/'
 app.get('/', (req, res) => {
-	res.render('index');
+	res.render('connexion');
+});
+// Quand le client demande '/'
+app.get('/i', (req, res) => {
+	res.render('inscription');
 });
 
 // Démarre le serveur (ecoute)
